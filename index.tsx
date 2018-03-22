af3cd8d39f4fed8e51c044805b654f2935f7fe78
Brian Demant <brian.demant@gmail.com>
2018-03-22 10:16:59 +0100

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,35 @@
+import * as React from "react"
+import { render } from "react-dom"
+import styled from "styled-components"
+import Button from "./Button"
+
+const Wrapper = styled.div`
+	padding: 20px;
+	background: #def;
+	width: 300px;
+
+	${Button} {
+		display: block;
+	}
+
+	${Button} ~ ${Button} {
+		margin-top: 20px;
+	}
+`
+
+const App = () => (
+	<Wrapper>
+		<Button>Click</Button>
+		<Button look="secondary" size="medium">
+			Click
+		</Button>
+		<Button look="dark" size="medium">
+			Click
+		</Button>
+		<Button look="light" size="huge" radius={100}>
+			Click
+		</Button>
+	</Wrapper>
+)
+
+render(<App />, document.getElementById("root"))
